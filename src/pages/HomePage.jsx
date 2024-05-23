31f3bba77c2348596a68daf5a26c97b405298488
Yann Duhamel <yann.duhamel@gmail.com>
2024-05-23 20:29:39 +0200

    feat: homepage done

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,20 @@
 export default function HomePage() {
-  return <></>;
+  return (
+    <div className="h-[77svh] w-[80svw] mx-auto flex flex-col items-center justify-evenly text-center">
+      <div className=" flex flex-col items-center relative text-center">
+        <img
+          src="../../public/images/duotone.jpg"
+          alt="photo de profil"
+          className="max-h-[40svh] rounded-full mx-auto"
+        />
+        <h1 className="font-migra text-7xl absolute bottom-[-9%]">Bonjour!</h1>
+      </div>
+      <div>
+        <h2 className="font-inter text-3xl">
+          Je suis <span className="font-bold">Yann</span>, développeur web
+          installé en <span className="font-bold">Bretagne</span>
+        </h2>
+      </div>
+    </div>
+  );
 }
