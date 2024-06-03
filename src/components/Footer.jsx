e542955ba356b06fa809eeb65555fa400922759b
Yann Duhamel <yann.duhamel@gmail.com>
2024-06-03 22:57:48 +0200

    feat: create project detail page structure + update json file

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <>
       <div className="flex justify-center">
-        <ul className="w-32 flex justify-evenly p-4 mt-4 gap-4 border-2 border-solid border-white rounded-full font-inter hover:border-saffron">
+        <ul className="w-32 flex justify-evenly p-4 my-4 gap-4 border-2 border-solid border-white rounded-full font-inter hover:border-saffron">
           <li>
             <NavLink
               to="https://www.linkedin.com/in/yannduhamel/"
--- src/pages/ProjectDetailPage.jsx
+++ src/pages/ProjectDetailPage.jsx
@@ -5,8 +5,44 @@ export default function ProjectDetailPage() {
   const { id } = useParams();
   const projectDetail = project.find((project) => project.id === id);
   return (
-    <>
-      <h1>Coucou {projectDetail.name} </h1>
-    </>
+    <div className="flex flex-col items-center mx-8">
+      <img
+        src={projectDetail.image}
+        alt="Image d'illustration du projet"
+        className="max-w-56 rounded-full mt-10"
+      />
+      <h1 className="mt-4 font-migra text-4xl text-saffron">
+        {projectDetail.name}
+      </h1>
+      <h2 className="mb-10">{projectDetail.type}</h2>
+      <p className="mb-4">{projectDetail.description}</p>
+      <div className="mb-4">
+        <p>
+          Le site:{" "}
+          <a
+            href={projectDetail.website}
+            target="_blank"
+            className="hover:text-saffron hover:font-bold"
+          >
+            par là!
+          </a>
+        </p>
+        <p>
+          Le repo:{" "}
+          <a
+            href={projectDetail.repo}
+            target="_blank"
+            className="hover:text-saffron hover:font-bold"
+          >
+            par ici!
+          </a>
+        </p>
+      </div>
+      <div className="flex gap-x-4 text-saffron font-bold">
+        <div>{projectDetail.stack.stack1}</div>
+        <div>{projectDetail.stack.stack2}</div>
+        <div>{projectDetail.stack.stack3}</div>
+      </div>
+    </div>
   );
 }
